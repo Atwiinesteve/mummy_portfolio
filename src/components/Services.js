1dc4f026570b4cf26d97e2cf0bb220171e41ded6
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2022-12-25 17:17:10 +0000

    edited services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,15 +7,21 @@ import "../styles/Services.css";
 
 function Services() {
 
+    // styles
+    const styles = {
+        width: "50px"
+    }
+
   return (
 		<>
 			{/* Services Section */}
 			<section className="services--area">
 				<div className="services--header">
 					<h1>services</h1>
+                    <hr style={ styles } />
 				</div>
 				<div className="services--brief--text">
-					<p>what I can offer to you</p>
+					<p>Some of the Services Available at our Offerings..</p>
 				</div>
 
 				{/* All Services */}
@@ -24,25 +30,25 @@ function Services() {
 						image="https://images.pexels.com/photos/371285/pexels-photo-371285.jpeg?auto=compress&cs=tinysrgb&w=600"
 						alt="people holding hands"
 						heading="Behavioral Therapy"
-                        text="Loving you was the best decision to be made in my life"
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 					/>
 					<Service
 						image="https://images.pexels.com/photos/652352/pexels-photo-652352.jpeg?auto=compress&cs=tinysrgb&w=600"
 						alt="people holding hands"
 						heading="Career Guidance"
-                        text="Loving you was the best decision to be made in my life"
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 					/>
 					<Service
 						image="https://images.pexels.com/photos/1415131/pexels-photo-1415131.jpeg?auto=compress&cs=tinysrgb&w=600"
 						alt="people holding hands"
 						heading="Relationships Counselling"
-                        text="Loving you was the best decision to be made in my life"
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 					/>
 					<Service
 						image="https://images.pexels.com/photos/984923/pexels-photo-984923.jpeg?auto=compress&cs=tinysrgb&w=600"
 						alt="people holding hands"
 						heading="Couple Counselling"
-                        text="Loving you was the best decision to be made in my life"
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 					/>
 				</div>
 			</section>
--- src/others/FeaturedServices.js
+++ src/others/FeaturedServices.js
@@ -3,7 +3,9 @@ import React from 'react'
 function FeaturedServices() {
   return (
     <div>
-      
+      <p>
+        lorem
+      </p>
     </div>
   )
 }
