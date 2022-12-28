271e045cf728ec2cbc8cea1dc2f598242f642f6d
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2022-12-28 19:45:49 +0000

    Added we-have-gat-you section

---
+++ src/components/WevaGatYou.js
@@ -0,0 +1,28 @@
+// import react
+import React from 'react';
+
+// importing styles.
+import "../styles/WevGatYou.css";
+
+
+function WevaGatYou() {
+
+  return (
+		<>
+			<section id="we--have--gat--you">
+				<img
+                    width="50px"
+					src="https://cdn-icons-png.flaticon.com/512/42/42760.png"
+					alt="smiling face"
+				/>
+				<div className="we--have--gat--you--content">
+					<h2>We've Got You</h2>
+				</div>
+			</section>
+		</>
+	);
+
+};
+
+// exporting module
+export default WevaGatYou
