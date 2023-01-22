85bc4ab94f17fce667099ff6c0e8563be96f94f1
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-01-22 15:34:57 +0000

    resized component spacing

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,39 +8,39 @@ import "../styles/ContactForm.css";
 function ContactForm() {
 
     return (
-
-        <>
-            
-            <section id="contact--form--area">
-                <div className="contact--form--header">
-                    <h1>Don't wait longer. Start forging your own path today.</h1>
-                </div>
-                <form autoComplete="off" >
-                    <div className="input--control">
-                        <label htmlFor='names'>full names</label><br/>
-                        <input type="text" />
-                    </div>
-                    <div className="input--control">
-                        <label>email</label><br/>
-                        <input type="email" />
-                    </div>
-                    <div className="input--control">
-                        <label>contact number</label><br/>
-                        <input type="tel" />
-                    </div>
-                    <div className="input--control">
-                        <label>message</label><br/>
-                        <textarea cols="30" rows="10"></textarea>
-                    </div>
-                    <div className="submit--button">
-                        <button type="submit" >send</button>
-                    </div>
-                </form>
-            </section>
-
-        </>
-
-    );
+			<>
+				<section id="contact--form--area">
+					<div className="contact--form--header">
+						<h1>Don't wait longer. Start forging your own path today.</h1>
+					</div>
+					<form autoComplete="off" action="https://formspree.io/f/moqbkyqe" method="POST">
+						<div className="input--control">
+							<label htmlFor="names">full names</label>
+							<br />
+							<input type="text" />
+						</div>
+						<div className="input--control">
+							<label>email</label>
+							<br />
+							<input type="email" />
+						</div>
+						<div className="input--control">
+							<label>contact number</label>
+							<br />
+							<input type="tel" />
+						</div>
+						<div className="input--control">
+							<label>message</label>
+							<br />
+							<textarea cols="30" rows="10"></textarea>
+						</div>
+						<div className="submit--button">
+							<button type="submit">send</button>
+						</div>
+					</form>
+				</section>
+			</>
+		);
 
 }
 
