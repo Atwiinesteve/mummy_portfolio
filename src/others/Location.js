81ce7976d7878f184ee0f695c5694fb7d020c470
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-06-08 10:51:19 +0000

    removed Location component

--- src/others/Location.js
+++ src/others/Location.js
@@ -1,25 +1,25 @@
-// importing components
-import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+// // importing components
+// import React, { Component } from "react";
+// import { Map, GoogleApiWrapper } from "google-maps-react";
 
-export class Location extends Component {
-	render() {
-		return (
-			<Map
-				style={{ position: "absolute", width: "100%", height: "70%" }}
-				google={this.props.google}
-				initialCenter={{
-					lat: 0.04069,
-					lng: 32.458561,
-				}}
-				zoom={15}
-			/>
-		);
-	}
-}
+// export class Location extends Component {
+// 	render() {
+// 		return (
+// 			<Map
+// 				style={{ position: "absolute", width: "100%", height: "70%" }}
+// 				google={this.props.google}
+// 				initialCenter={{
+// 					lat: 0.04069,
+// 					lng: 32.458561,
+// 				}}
+// 				zoom={15}
+// 			/>
+// 		);
+// 	}
+// }
 
-// exporting Location module.
-export default GoogleApiWrapper({
-	apiKey: "AIzaSyBmdWULlrQTdldi-FloVyzcMsxTq0An_SM",
-})(Location);
+// // exporting Location module.
+// export default GoogleApiWrapper({
+// 	apiKey: "AIzaSyBmdWULlrQTdldi-FloVyzcMsxTq0An_SM",
+// })(Location);
 
